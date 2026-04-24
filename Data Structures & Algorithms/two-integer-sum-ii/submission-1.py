fc70d5356adc01f6d8a95b7fcb7e5b274e4c5d49
Usman Khaliq <usman@overjet.ai>
2026-04-24 16:27:02 -0700

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,19 @@
+# two pointers 
+# one pointer at start , other at end
+# at each iteration, while l < r, if sum is greater than target, move right pointer left
+# else move left pointer right
+# return when sum is same as target
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:  
+        l, r = 0, len(numbers) - 1 
+        while l < r:
+            currentSum = numbers[l] + numbers[r]
+            if currentSum > target:
+                r -= 1 
+            elif currentSum < target:
+                l += 1 
+            else:
+                return [l + 1, r + 1]
+        
+        
